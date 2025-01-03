b18c68b83905923ac83e1bdd6050bef142358921
HelioMelo <Helio_melo_@hotmail.com>
2025-01-03 15:23:08 -0300

    feat: create user

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/createUser.dto';
+
+@Controller('user')
+export class UserController {
+  @Post()
+  async createUser(@Body() createUser: CreateUserDTO) {
+    return {
+      ...createUser,
+      password: undefined,
+    };
+  }
+}
